a20477733f09b3b7465f977a3c93c1210f1c823f
z <kpro8899@protonmail.com>
2025-04-19 21:36:21 +0800

    refactor: 排版

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,3 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
@@ -18,27 +9,36 @@ defineProps({
   </div>
 </template>
 
+<script setup lang="ts">
+  defineProps({
+    msg: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+  h1 {
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
+  }
 
-h3 {
-  font-size: 1.2rem;
-}
+  h3 {
+    font-size: 1.2rem;
+  }
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
-    text-align: left;
+    text-align: center;
   }
-}
+
+  @media (min-width: 1024px) {
+    .greetings h1,
+    .greetings h3 {
+      text-align: left;
+    }
+  }
 </style>
